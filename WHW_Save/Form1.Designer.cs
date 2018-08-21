b55bdb2d1f77154c89d9784a0af5e37918bcdd3a
YoZengWeijie <330564052@qq.com>
2018-08-21 11:37:08 +0800

    初始提交

---
+++ WHW_Save/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WHW_Save
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.simpleButton4);
+            this.panelControl1.Controls.Add(this.simpleButton3);
+            this.panelControl1.Controls.Add(this.simpleButton2);
+            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Controls.Add(this.textEdit2);
+            this.panelControl1.Controls.Add(this.labelControl2);
+            this.panelControl1.Controls.Add(this.textEdit1);
+            this.panelControl1.Controls.Add(this.labelControl1);
+            this.panelControl1.Location = new System.Drawing.Point(12, 12);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(361, 197);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Appearance.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
+            this.simpleButton4.Appearance.Options.UseFont = true;
+            this.simpleButton4.Location = new System.Drawing.Point(7, 122);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(160, 44);
+            this.simpleButton4.TabIndex = 4;
+            this.simpleButton4.Text = "保存配置";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Appearance.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Bold);
+            this.simpleButton3.Appearance.Options.UseFont = true;
+            this.simpleButton3.Location = new System.Drawing.Point(192, 122);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(160, 44);
+            this.simpleButton3.TabIndex = 3;
+            this.simpleButton3.Text = "上传存档";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(277, 64);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "确定";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(277, 19);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "选择";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(97, 65);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(174, 20);
+            this.textEdit2.TabIndex = 1;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(7, 64);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(84, 20);
+            this.labelControl2.TabIndex = 0;
+            this.labelControl2.Text = "存档接收邮箱";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(97, 20);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(174, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(7, 19);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(84, 20);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "选择存档路径";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "存档文件|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 222);
+            this.Controls.Add(this.panelControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MHW_Save";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+    }
+}
+
---
+++ WHW_Save/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
+
+namespace WHW_Save
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        static SmtpClient smt;
+        public bool flag = true;
+        public string path = Environment.CurrentDirectory + @"\Config.ini";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void MakeSmtpConnection()
+        {
+            smt = new SmtpClient();
+            smt.Host = "smtp.qq.com";
+            smt.EnableSsl = true;
+            smt.Credentials = new NetworkCredential("330564052@qq.com", "ovysprmezglkbifi");
+        }
+
+        static void SendMails(string Address, string FilePath)
+        {
+            var mm = new MailMessage();
+            mm.From = new MailAddress("330564052@qq.com", "MHW_Save", Encoding.UTF8);
+            mm.To.Add(new MailAddress("330564052@qq.com"));
+            mm.BodyEncoding = Encoding.UTF8;
+            mm.Subject = "Monster Hunter: World_Save";
+            mm.Attachments.Add(new Attachment(FilePath));
+            SendMail(mm);
+        }
+
+        static void SendMail(MailMessage ms)
+        {
+            smt.Send(ms);
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                textEdit1.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                textEdit2.Enabled = false;
+                flag = false;
+            }
+            else
+            {
+                textEdit2.Enabled = true;
+                flag = true;
+            }
+
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textEdit1.Text != "" && textEdit2.Text != "")
+                {
+                    MakeSmtpConnection();
+                    string eMail = textEdit2.Text;
+                    string adress = textEdit1.Text;
+                    SendMails(eMail, adress);
+                    MessageBox.Show("上传存档成功！");
+                    smt.Dispose();
+                    Thread.Sleep(1000);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("路径或邮箱不能为空！");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("上传失败，请检查路径或邮箱是否填写正确");
+            }
+
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            
+            IniFile ini = new IniFile(path);
+            if (textEdit1.Text != "" && textEdit2.Text != "")
+            {
+                ini.IniWriteValue("ini", "path", textEdit1.Text);
+                ini.IniWriteValue("ini", "eMail", textEdit2.Text);
+                MessageBox.Show("保存配置成功");
+                FileInfo info = new FileInfo(path);
+                if (info.Exists)
+                {
+                    info.Attributes = FileAttributes.Hidden;
+                }
+            }
+            else
+            {
+                MessageBox.Show("路径或邮箱不能为空！");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IniFile ini = new IniFile(path);
+            if (ini.ExistINIFile(path))
+            {
+                textEdit1.Text = ini.IniReadValue("ini", "path");
+                textEdit2.Text = ini.IniReadValue("ini", "eMail");
+            }
+            else
+            {
+                MessageBox.Show("未找到配置文件，请开始配置");
+            }
+           
+        }
+    }
+
+    public class IniFile
+    {
+        public string Path;
+
+        public IniFile(string path)
+        {
+            this.Path = path;
+        }
+
+        #region 声明读写INI文件的API函数
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defVal, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defVal, Byte[] retVal, int size, string filePath);
+
+        #endregion
+
+        /// <summary>
+        /// 写INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <param name="iValue">值</param>
+        public void IniWriteValue(string section, string key, string iValue)
+        {
+            WritePrivateProfileString(section, key, iValue, this.Path);
+        }
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">段落</param>
+        /// <param name="key">键</param>
+        /// <returns>返回的键值</returns>
+        public string IniReadValue(string section, string key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+
+            int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path);
+            return temp.ToString();
+        }
+
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="Section">段，格式[]</param>
+        /// <param name="Key">键</param>
+        /// <returns>返回byte类型的section组或键值组</returns>
+        public byte[] IniReadValues(string section, string key)
+        {
+            byte[] temp = new byte[255];
+
+            int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path);
+            return temp;
+        }
+
+        public bool ExistINIFile(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+
+}
